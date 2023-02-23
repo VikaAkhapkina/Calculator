1a5be828e6e10b24b550f8d9557a9835a6ca52b0
VikaAkhapkina <akhapkina_viktoriya@mail.ru>
2023-02-23 14:04:59 +0300

    added button cell

---
+++ Calculator/CalcController.swift
@@ -0,0 +1,88 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Viktoriya on 13.02.23.
+//
+
+import UIKit
+import SnapKit
+
+class CalcController: UIViewController {
+    
+    let viewModel: CalcControllerViewModel
+    
+    // MARK: - UI Components (Компоненты пользовательского интерфейса)
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .black
+        collectionView.register(CalcHeadeCell.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: CalcHeadeCell.identifier)
+        collectionView.register(ButtonCell.self, forCellWithReuseIdentifier: ButtonCell.identifier)
+        return collectionView
+    }()
+
+    
+    // MARK: - Lifecycle (Жизненный цикл)
+    init(viewModel: CalcControllerViewModel = CalcControllerViewModel()) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+        setupUI()
+        
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        
+    }
+    
+    // MARK: - UI Setup (Настройка пользовательского интерфейса)
+    private func setupUI() {
+        view.addSubview(self.collectionView)
+        
+        collectionView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+            $0.top.bottom.equalToSuperview()
+        }
+    }
+
+
+}
+// MARK: - CollectionView Methods (Методы CollectionView)
+extension CalcController: UICollectionViewDataSource {
+    
+    // MARK: - Normal Cells (Buttons)
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.viewModel.calcButtonCells.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ButtonCell.identifier, for: indexPath) as? ButtonCell else {
+            fatalError("Failed to dequeue ButtonCell in CalcController ")
+        }
+        let calcButton = self.viewModel.calcButtonCells[indexPath.row]
+        cell.configure(with: calcButton)
+        return cell
+    }
+    
+}
+
+extension CalcController: UICollectionViewDelegate {
+    
+}
+
+extension CalcController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width/5, height: view.frame.width/5)
+    }
+}
